abe3c5a637eab698ae6e9dae2966c7d49bedde60
Markov Andrey <andremarkov@icloud.com>
2022-11-21 16:55:32 +0300

    add pages

--- pages/scripts/header.js
+++ pages/scripts/header.js
@@ -1,11 +1,11 @@
 document.write(`
 <header>
-    <img src="img/logo.png" alt="Logo" class="logo1">
-    <img src="img/mlh-logo.png" alt="Logo" class="logo2">
+    <img src="../img/logo.png" alt="Logo" class="logo1">
+    <img src="../img/mlh-logo.png" alt="Logo" class="logo2">
       <h4>&nbsp;БЕЛГОСОХОТА&nbsp;</h4>
       <h3>Калькулятор</h3>
       <h3>охотничьих трофеев</h3>
-      <h3>&nbsp;</h3>
+      <h3>(${animal})</h3>
       <h5></h5>
 </header>
 `)
