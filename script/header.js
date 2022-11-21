180bb5557c9dabd9e85b005bf803ff93c634deda
Markov Andrey <andremarkov@icloud.com>
2022-11-21 16:27:44 +0300

    start work, index sheet complete

---
+++ script/header.js
@@ -0,0 +1,10 @@
+document.write(`
+<header>
+    <img src="img/logo.png" alt="Logo" class="logo1">
+    <img src="img/mlh-logo.png" alt="Logo" class="logo2">
+        <h4>&nbsp;БЕЛГОСОХОТА&nbsp;</h4>
+          <h3>Калькулятор</h3>
+          <h3>охотничьих трофеев</h3>
+          <h5></h5>
+</header>
+`)
